95b818ff6f6e75ef611a2e89032f7e5b1d1970f3
biggifraley <biggi.fraley@usa.net>
2016-10-24 14:18:19 -0700

    Add cursor loader to catalog activity

--- app/src/main/java/com/example/android/inventory/CatalogActivity.java
+++ app/src/main/java/com/example/android/inventory/CatalogActivity.java
@@ -1,12 +1,33 @@
 package com.example.android.inventory;
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class CatalogActivity extends AppCompatActivity {
+import com.example.android.inventory.data.ProductContract.ProductEntry;
+
+/**
+ * Displays list of products that were entered and stored in the app.
+ */
+public class CatalogActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    // Identifies a particular Loader being used in this component
+    private static final int PRODUCT_LOADER = 0;
+
+    // Create a new adapter that takes the list of products as inputs
+    ProductCursorAdapter mCursorAdapter;
+
+    public static final String LOG_TAG = CatalogActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +43,74 @@ public class CatalogActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // Find the ListView which will be populated with the pet data
+        ListView petListView = (ListView) findViewById(R.id.list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        petListView.setEmptyView(emptyView);
+
+        // Create a new adapter that takes the list of products as inputs
+        mCursorAdapter = new ProductCursorAdapter(this, null);
+
+        // Set the adapter on the {@link ListView}
+        // so the list can be populated in the user interface
+        petListView.setAdapter(mCursorAdapter);
+
+        petListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Create new intent to go to {@link EditorActivity}
+                Intent intent = new Intent(CatalogActivity.this, EditorActivity.class);
+                // Form the content URI to represent the specific product that was clicked on,
+                // by appending the "id" (passed as input to this method onto the
+                // {@link PetEntry#CONTENT_URI}.
+
+                Uri currentPetUri = ContentUris.withAppendedId(ProductEntry.CONTENT_URI, id);
+                // Set the URI on the data field of the intent
+                intent.setData(currentPetUri);
+                // Launch the {@link EditorActivity} to display the data for the current pet.
+                startActivity(intent);
+            }
+        });
+
+        // Prepare the loader.  Either re-connect with an existing one,
+        // or start a new one.
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
+    }
+
+    // Called when a new Loader needs to be created
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        // Define a projection that specifies the columns from the products table.
+        String[] projection = {
+                ProductEntry._ID,
+                ProductEntry.COLUMN_PRODUCT_NAME,
+                ProductEntry.COLUMN_PRODUCT_QUANTITY,
+                ProductEntry.COLUMN_PRODUCT_PRICE
+        };
+        // Now create and return a CursorLoader that will take care of
+        // creating a Cursor for the data being displayed.
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this, ProductEntry.CONTENT_URI, projection, null, null, null);
+    }
+
+    // Called when a previously created loader has finished loading
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Swap the new cursor in.  (The framework will take care of closing the
+        // old cursor once we return.)
+        mCursorAdapter.swapCursor(data);
+    }
+
+    // Called when a previously created loader is reset, making the data unavailable
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        // This is called when the last Cursor provided to onLoadFinished()
+        // above is about to be closed.  We need to make sure we are no
+        // longer using it.
+        mCursorAdapter.swapCursor(null);
     }
 }
